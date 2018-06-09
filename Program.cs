f61c3b61121232bf4ed3dfd7e51d7edc47b26932
paddi <paddim8@gmail.com>
2018-06-09 23:56:41 +0200

    Hello World!

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DiscordBotBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
